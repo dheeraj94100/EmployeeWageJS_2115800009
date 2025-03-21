81a6e671a46dfd5b37343df847d421af602f5116
Dheeraj Sharma <dheeraj9410096874@gmail.com>
2025-03-21 15:19:21 +0530

    [Dheeraj Sharma] Add: UC5 implemented.

--- app.js
+++ app.js
@@ -57,3 +57,27 @@ for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
 }
 empWage = empHrs * WAGE_PER_HOUR;
 console.log("Total Hrs: " + empHrs + " Emp Wage: " + empWage);
+
+// UC5
+const MAX_HOURS_IN_MONTH = 100;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+
+while (
+  totalEmpHrs < MAX_HOURS_IN_MONTH &&
+  totalWorkingDays < NUM_OF_WORKING_DAYS
+) {
+  totalWorkingDays++;
+  let empCheck = Math.floor(Math.random() * 10) % 3;
+  totalEmpHrs += getWorkingHours(empCheck);
+}
+
+empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log(
+  "Total Days: " +
+    totalWorkingDays +
+    " Total Hrs: " +
+    totalEmpHrs +
+    " Emp Wage: " +
+    empWage
+);
